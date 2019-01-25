529d9f8c5043c25976495bd2c5e6382afe6a3f78
cheferrari <34338482+cheferrari@users.noreply.github.com>
2019-01-25 17:05:47 +0800

    Update to k8s1.13.2

--- pull-k8s-images.sh
+++ pull-k8s-images.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# 拉取kubeadm1.12.0初始化所需镜像，https://hub.docker.com/u/anjia0532/，https://github.com/anjia0532/gcr.io_mirror
-images=(kube-proxy:v1.12.0 kube-scheduler:v1.12.0 kube-controller-manager:v1.12.0 kube-apiserver:v1.12.0 etcd:3.2.24 coredns:1.2.2 pause:3.1 )
+VERSION=v1.13.2
+# 拉取kubeadm1.13.2初始化所需镜像，mirrorgooglecontainers  https://hub.docker.com/u/anjia0532/，https://github.com/anjia0532/gcr.io_mirror
+# 运行 kubeadm config images list 命令可查看该版本kubeadm 所需那些镜像及版本信息
+images=(kube-proxy:$VERSION kube-scheduler:$VERSION kube-controller-manager:$VERSION kube-apiserver:$VERSION etcd:3.2.24 coredns:1.2.2 pause:3.1 )
 for imageName in ${images[@]} ; do
-docker pull anjia0532/google-containers.$imageName
-docker tag anjia0532/google-containers.$imageName k8s.gcr.io/$imageName
-docker rmi anjia0532/google-containers.$imageName
+docker pull mirrorgooglecontainers/$imageName
+docker tag mirrorgooglecontainers/$imageName k8s.gcr.io/$imageName
+docker rmi mirrorgooglecontainers/$imageName
 done
 
 # kubeadm 初始化 k8s 集群，master节点执行，https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/#instructions
-# kubeadm init --kubernetes-version=v1.12.0 --pod-network-cidr=10.244.0.0/16
+# kubeadm init --kubernetes-version=v1.13.2 --pod-network-cidr=10.244.0.0/16
