773034b50bdb822f520e2ab6f03be2a7d5874a27
cheferrari <34338482+cheferrari@users.noreply.github.com>
2019-05-25 15:15:54 +0800

    Update to v1.14.2

--- k8s-env-setting-up.sh
+++ k8s-env-setting-up.sh
@@ -123,8 +123,8 @@ systemctl daemon-reload
 systemctl enable docker && systemctl restart docker
 
 # Installing kubeadm, kubelet and kubectl
-# 安装指定版本的 kubeadm, 默认 1.14.1 版本
+# 安装指定版本的 kubeadm, 默认 1.14.2 版本
 # yum list kubeadm --showduplicates
-# yum install -y kubelet-1.14.1 kubeadm-1.14.1 kubectl-1.14.1
-yum install -y kubelet-${K8S_VERSION:-1.14.1} kubeadm-${K8S_VERSION:-1.14.1} kubectl-${K8S_VERSION:-1.14.1} --disableexcludes=kubernetes
+# yum install -y kubelet-1.14.2 kubeadm-1.14.2 kubectl-1.14.2
+yum install -y kubelet-${K8S_VERSION:-1.14.2} kubeadm-${K8S_VERSION:-1.14.2} kubectl-${K8S_VERSION:-1.14.2} --disableexcludes=kubernetes
 systemctl enable kubelet && systemctl start kubelet
